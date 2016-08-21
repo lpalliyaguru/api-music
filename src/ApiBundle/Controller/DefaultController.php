ff37267537acbbe2c6c30c47dde07b3079fa58e8
Manoj Lasantha <manoj.lasantha@insead.edu>
2016-08-22 00:13:43 +0800

    added search endpoint

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -2,16 +2,40 @@
 
 namespace ApiBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     /**
-     * @Route("/")
+     * @Rest\Get("/")
      */
     public function indexAction()
     {
         return $this->render('ApiBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Rest\Options("/search")
+     */
+    public function optionsSearchAction(Request $request)
+    {}
+
+    /**
+     * @Rest\Get("/search")
+     */
+    public function getSearchAction(Request $request)
+    {
+        $keyword = $request->query->get('search');
+        $artistManager = $this->get('manager.artist');
+        $albumManager = $this->get('manager.album');
+        $artists = $artistManager->search($keyword);
+
+        return array(
+            'success'   => true,
+            'data'      => $artists
+        );
+    }
+
 }
